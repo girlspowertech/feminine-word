e04ef3eb99f474bac191f9fb1d72b0b5ecda9649
anig1scur <anigiscur@gmail.com>
2024-07-13 00:22:51 +0800

    deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === '1' ? '/feminine-word/' : '/',
   plugins: [react()],
   resolve: {
     alias: [
